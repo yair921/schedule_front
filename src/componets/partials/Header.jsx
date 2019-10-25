e689465bdfc7dc28474404bdd81a501dcf544f29
Yair <yair_918@hotmail.com>
2019-10-24 21:27:59 -0500

    Add dropdown in menu

--- src/componets/partials/Header.jsx
+++ src/componets/partials/Header.jsx
@@ -18,27 +18,37 @@ class Header extends Component {
 
     render() {
         return (
-            <nav>
-                <div className="nav-wrapper">
-                    <a href="#/welcome" className="brand-logo">Schedule</a>
-                    <a href="#" data-target="responsive-menu" className="sidenav-trigger">
-                        <i className="material-icons">menu</i>
-                    </a>
+            <div>
+                <ul id="dropdown1" className="dropdown-content">
+                    <li><a href="#/schedule">Programación</a></li>
+                    <li><a href="#!">two</a></li>
+                    <li className="divider"></li>
+                    <li><a href="#!">three</a></li>
+                </ul>
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="#/welcome" className="brand-logo">Schedule</a>
+                        <a href="#" data-target="responsive-menu" className="sidenav-trigger">
+                            <i className="material-icons">menu</i>
+                        </a>
 
-                    <ul ref={this.onRef} className="sidenav" id="responsive-menu">
-                        <li><a href="#/schedule">Programación</a></li>
-                        <li><a href="#">Teatros</a></li>
-                        <li><a href="#">Películas</a></li>
-                        <li><a href="#">Usuarios</a></li>
-                    </ul>
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="#/schedule">Programación</a></li>
-                        <li><a href="#">Teatros</a></li>
-                        <li><a href="#">Películas</a></li>
-                        <li><a href="#">Usuarios</a></li>
-                    </ul>
-                </div>
-            </nav>
+                        <ul ref={this.onRef} className="sidenav" id="responsive-menu">
+                            <li><a href="#">Programación</a></li>
+                            <li><a href="#">Teatros</a></li>
+                            <li><a href="#">Películas</a></li>
+                            <li><a href="#">Usuarios</a></li>
+                            <li><a className="dropdown-trigger" href="#" data-target="dropdown1">Programación<i className="material-icons right">arrow_drop_down</i></a></li>
+                        </ul>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            <li><a href="#/schedule">Programación</a></li>
+                            <li><a href="#">Teatros</a></li>
+                            <li><a href="#">Películas</a></li>
+                            <li><a href="#">Usuarios</a></li>
+                            <li><a className="dropdown-trigger" href="#" data-target="dropdown1">Programación<i className="material-icons right">arrow_drop_down</i></a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         );
     }
 }
